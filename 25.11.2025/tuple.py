0a19451fe283e1bc994600cecb516053c757179d
Ayushi100503 <Ayushi.Chakraborty@gds.ey.com>
2025-11-25 07:28:14 +0000

    s

---
+++ 25.11.2025/tuple.py
@@ -0,0 +1,32 @@
+numbers = (10,20,30)
+names = ("ayushi", "sriparna","ankur")
+mixed = (10, "hello", 3.5, True)
+
+fruits = ("apple", "banana", "kiwi")
+print(fruits[0])
+print(fruits[1])
+print(fruits[-1])
+
+
+num =(1,2,3,2)
+print(num.count(2))
+
+letter = ('a','b','c','d')
+print(letter.index('c'))
+
+nums = (10,20,30)
+print(nums[1:4])
+print(nums[2:4])
+print(nums[::-1])
+
+
+#packing and unpacking
+data = 10, 20, 30
+a, b, c = data
+print(a, b, c)
+
+
+employee = ("ayushi", 22, ("kolkata", "india"))
+print(employee[2][0])
+
+
